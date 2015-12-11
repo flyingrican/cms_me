5c8acad433eedf055be32fd5bd86a2c6e175003f
flyingrican <kortiz1514@gmail.com>
2015-12-11 12:35:24 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php include "db.php"; ?>
+<?php session_start(); ?>
+<?php include "../functions.php"; ?>
+
+<?php 
+
+    if(isset($_POST['login'])){
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        $username = mysqli_real_escape_string($connection, $username);
+        $password = mysqli_real_escape_string($connection, $password);
+        
+        $query = "SELECT * FROM users WHERE username = '{$username}'";
+        $select_user_query = mysqli_query($connection, $query);
+        
+        confirm($select_user_query);
+
+    }
+
+    while($row = mysqli_fetch_array($select_user_query)){
+        
+        $db_id = $row['user_id'];
+        $db_user_username = $row['username'];
+        $db_user_password = $row['user_password'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role = $row['user_role'];
+        
+        
+    }
+
+    //$password = crypt($password, $db_user_password);
+
+    if(password_verify($password, $db_user_password)){
+        
+        $_SESSION['user_id'] = $db_id;
+        $_SESSION['username'] = $db_user_username;
+        $_SESSION['firstname'] = $db_user_firstname;
+        $_SESSION['lastname'] = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+        
+        header("Location: ../admin");
+        
+    }else{
+        header("Location: ../index.php");
+    }
+
+
+
+?>
